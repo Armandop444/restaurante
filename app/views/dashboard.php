7ba291b5245c009c794c98f3dc442a4ddef267be
oc18018 <oc18018@ues.edu.sv>
2023-01-17 14:37:16 -0600

    correccion el viewport

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=devide-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>public_html/css/bootstrap.min.css">
     <title>Restaurante</title>
 </head>
