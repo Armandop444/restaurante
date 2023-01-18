02e7efcef17f1ebab1a77962140191cf8d481690
oc18018 <oc18018@ues.edu.sv>
2023-01-18 11:35:49 -0600

    cdn de iconos añadido

--- app/views/components/css.php
+++ app/views/components/css.php
@@ -1,5 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>public_html/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 <title>Restaurante</title>
 <link rel="shortcut icon" href="<?php echo URL; ?>public_html/img/logo.png">
--- app/views/components/login.php
+++ app/views/components/login.php
@@ -2,5 +2,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-0 mr-4">
     <div class="navbar-collapse" id="navbarSupportedContent">
     </div>
-    <h6 class="text-white"><?php echo $_SESSION["nuser"]?>(<a class="btn text-primary" href="<?php echo URL?>login/cerrar">Salir</a>)</h6>
+    <h6 class="text-white"><i class="bi bi-person"></i><?php echo $_SESSION["nuser"]?>(<a class="btn text-primary" href="<?php echo URL?>login/cerrar">Salir</a>)</h6>
 </nav>
